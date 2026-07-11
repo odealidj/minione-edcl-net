179c07be223bbe5c5d62d016e80f661cb93ee5c5
odealidj <odealidj.go@gmail.com>
2026-07-11 11:11:07 +0700

    fix: update frontend API URL for web-mobile separation

--- fe/web/src/environments/environment.ts
+++ fe/web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5140/api/v1' // Replace with production URL later
+  apiUrl: 'http://localhost:5140/api/v1/web' // Replace with production URL later
 };
