d67aa60a3b3a1e04352419579b9a79227d04c4ca
odealidj <odealidj.go@gmail.com>
2026-08-08 16:27:16 +0700

    chore: remove scratch files from repo and gitignore them

--- test_db.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Data.SqlClient;
-using Dapper;
-
-var connStr = "Server=localhost,1433;Database=EDCL;User Id=sa;Password=Password123!;TrustServerCertificate=true;";
-using var conn = new SqlConnection(connStr);
-var countSync = conn.QuerySingle<int>("SELECT COUNT(*) FROM ingestion.sync_sessions");
-var countPickup = conn.QuerySingle<int>("SELECT COUNT(*) FROM job.pickup_orders");
-Console.WriteLine($"SyncSessions: {countSync}, PickupOrders: {countPickup}");
